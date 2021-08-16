3a6f10b16d92ec2c249ab0e9eb6393cad8beba1b
Pablo Gutiérrez <be.legal.copy.linux@gmail.com>
2021-08-16 02:30:37 +0200

    Auto-reload Freqtrade bot after 120s last commit download
    Auto-reload Freqtrade bot after 120s last commit download

--- NostalgiaForInfinity_Update.sh
+++ NostalgiaForInfinity_Update.sh
@@ -6,7 +6,6 @@ FT_PATH="${ROOT_PATH}/freqtrade/user_data/strategies/NostalgiaForInfinityNext.py
 TG_TOKEN=""
 TG_CHAT_ID=""
 
-# Go to NFI directory
 cd $(dirname ${NFI_PATH})
 
 GITRESPONSE=`git pull`
@@ -15,7 +14,20 @@ UPDATED='Already up to date.'
 if [[ $GITRESPONSE != $UPDATED ]]; then
         cp NostalgiaForInfinityNext.py ${FT_PATH}
 
-        curl -s --data "text=There is a new NFI commit. Please \`/reload_config\` to get it loaded." \
+        curl -s --data "text=New NFI commit! Please wait for reload..." \
+                --data "parse_mode=markdown" \
+                --data "chat_id=$TG_CHAT_ID" \
+                "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+
+        sleep 120
+
+        cd $(dirname ${FT_PATH})
+        docker-compose down > /dev/null &&
+        docker-compose build --pull > /dev/null &&
+        docker-compose up -d --remove-orphans > /dev/null &&
+        docker system prune --volumes -af > /dev/null
+
+        curl -s --data "text=NFI reload has been completed!" \
                 --data "parse_mode=markdown" \
                 --data "chat_id=$TG_CHAT_ID" \
                 "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
